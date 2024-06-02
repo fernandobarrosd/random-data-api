ed6249caa1c0bb1e0c393d5e8015a1a3e62d6461
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-02 12:39:42 -0300

    Update CPFRandomGenerator

--- src/main/java/com/fernando/random_data_api/generators/CPFRandomGenerator.java
+++ src/main/java/com/fernando/random_data_api/generators/CPFRandomGenerator.java
@@ -10,37 +10,33 @@ import com.fernando.random_data_api.utils.RandomUtils;
 public class CPFRandomGenerator implements RandomGenerator<Boolean, String> {
     @Autowired
     private Random random;
+
+    private StringBuilder randomCPFBuilder;
+
+    private Character[] numbers = new Character[11];
+
     private static final Character[] NUMBERS = {
         '0', '1', '2', '3', '4','5', '6', '7', '8', '9'
     };
 
     @Override
     public String generateRandom(Boolean hasEspecialCharacters) {
-        StringBuilder randomCPFBuilder = new StringBuilder();
-
-        Character[] numbers = new Character[11];
-
         for (int i = 0; i < numbers.length; i ++) {
             numbers[i] = randomNumber();
         }
         
         if (hasEspecialCharacters) {
-            randomCPFBuilder.append(numbers[0]);
-            randomCPFBuilder.append(numbers[1]);
-            randomCPFBuilder.append(numbers[2]);
-            randomCPFBuilder.append(".");
-            randomCPFBuilder.append(numbers[3]);
-            randomCPFBuilder.append(numbers[4]);
-            randomCPFBuilder.append(numbers[5]);
-            randomCPFBuilder.append(".");
-            randomCPFBuilder.append(numbers[6]);
-            randomCPFBuilder.append(numbers[7]);
-            randomCPFBuilder.append(numbers[8]);
+            randomCPFBuilder = new StringBuilder();
+            appendNumbersAndPointToCPFStringBuilder(0, 3, false);
+            appendNumbersAndPointToCPFStringBuilder(3, 6, false);
+            appendNumbersAndPointToCPFStringBuilder(6, 9, true);
+            
             randomCPFBuilder.append("-");
             randomCPFBuilder.append(numbers[9]);
             randomCPFBuilder.append(numbers[10]);
         }
         else {
+            randomCPFBuilder = new StringBuilder();
             for (int i = 0; i < numbers.length; i ++) {
                 var number = numbers[i];
                 randomCPFBuilder.append(number);
@@ -50,8 +46,24 @@ public class CPFRandomGenerator implements RandomGenerator<Boolean, String> {
         
     }
 
+    private void appendNumbersAndPointToCPFStringBuilder(Integer initialValue, Integer numbersCount, Boolean isLast) {
+        for (int i = initialValue; i < numbersCount; i ++) {
+            randomCPFBuilder.append(numbers[i]);
+        }
+
+        if (!isLast) {
+            randomCPFBuilder.append(".");
+        }
+    }
+
+    
+
     private Character randomNumber() {
         return RandomUtils.randomValue(random, NUMBERS);
     }
+
+    
+
+    
 
 }
