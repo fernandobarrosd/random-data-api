33fd50f52a59855a01f72849440b0b62d71d6741
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-04 19:47:50 -0300

    Update getRandomText HTTP method type to POST

--- src/main/java/com/fernando/random_data_api/controllers/RandomController.java
+++ src/main/java/com/fernando/random_data_api/controllers/RandomController.java
@@ -3,6 +3,7 @@ package com.fernando.random_data_api.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,7 +45,7 @@ public class RandomController {
         return new RandomPasswordResponse(passwordSize, randomPassword);
     }
 
-    @GetMapping("/text")
+    @PostMapping("/text")
     public RandomTextResponse getRandomText(@RequestBody RandomTextRequestBody randomTextRequestBody) {
         List<String> texts = randomTextRequestBody.texts();
         if (texts == null) {
